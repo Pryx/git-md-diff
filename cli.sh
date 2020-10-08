faa0b94a4a2685e63fc8861ea4a48bc9585655db
Vojtěch Sajdl <vojtech@sajdl.com>
2020-10-08 12:43:51 +0200

    Fix #5 - Upload to drive

--- cli.sh
+++ cli.sh
@@ -3,14 +3,23 @@
 # Help menu
 print_help() {
 	cat <<-HELP
-	
+Usage:
+./cli.sh -D="~/repository" -S=HEAD~20 -E=HEAD~10
+
+  -D | --repository_dir      Path of repository you want to run diff on.
+  -S | --start               First commit of diff
+  -E | --end                 Last commit of diff. HEAD if left empty
+  -d | --debug               Prints more verbose output
+  -u | --upload              Upload to google drive
+  -h | --help                Prints this help
 	HELP
 	exit 0
 }
 repository_dir="."
 start="HEAD~1"
 end="HEAD"
-debug=false
+verbose=false
+upload=false
 
 # Parse Command Line Arguments
 while [ "$#" -gt 0 ]; do
@@ -24,10 +33,13 @@ while [ "$#" -gt 0 ]; do
     --end=|-E=*)
         end="${1#*=}"
         ;;
-    --debug=|-d=*)
-        debug="${1#*=}"
+    --verbose=|-v=*)
+        verbose="${1#*=}"
         ;;
-    --help) print_help;;
+    --upload|-u*)
+        upload=true
+        ;;
+    --help|-h) print_help;;
     *)
       printf "***********************************************************\n"
       printf "* Error: Invalid argument, run --help for valid arguments. *\n"
@@ -51,8 +63,12 @@ fi
 mkdir ./out
 mkdir ./tmp
 
-if [ "$debug" = true ] ; then
-    echo "DEBUG MODE ACTIVE"
+if [ "$verbose" = true ] ; then
+    echo "VERBOSE MODE ACTIVE"
+fi
+
+if [ "$upload" = true ] ; then
+    node -r esm upload.js cleanup
 fi
 
 git --git-dir="${repository_dir}/.git" diff-tree -r --no-renames "${start}".."${end}" | \
@@ -76,11 +92,21 @@ git --git-dir="${repository_dir}/.git" diff-tree -r --no-renames "${start}".."${
 		        	git --git-dir="${repository_dir}/.git" show "$srcsha" > "./tmp/${srcsha}"
 			        git --git-dir="${repository_dir}/.git" show "$dstsha" > "./tmp/${dstsha}"
 
-			        node -r esm diff.js run -f="./tmp/${srcsha}" -m="./tmp/${dstsha}" -o="./out/${dstsha}.html"
+                    if [ "$upload" = true ] ; then
+                        node -r esm diff.js compile -f="./tmp/${srcsha}" -m="./tmp/${dstsha}"
+
+                        node -r esm upload.js upload -f="./tmp/${srcsha}.html" -n="${filename}" -o
+
+                        node -r esm upload.js upload -f="./tmp/${dstsha}.html" -n="${filename}"
+                    else
+		    	        node -r esm diff.js run -f="./tmp/${srcsha}" -m="./tmp/${dstsha}" -o="./out/${dstsha}.html"
+                    fi
 		        fi
 		    ;;
 		    *)
-	        echo "$filename has unsupported extension"
+            if [ "$verbose" = true ] ; then
+	            echo "$filename has unsupported extension"
+            fi
 	        ;;
 		esac
 
--- diff.js
+++ diff.js
@@ -3,7 +3,7 @@ import { markdownDiff } from 'markdown-diff';
 import yargs from 'yargs';
 
 var argv = yargs
-  .scriptName("diff")
+  .scriptName("diff.js")
   .usage('Usage: $0 <command> [args]')
   .command(
     'run',
@@ -21,6 +21,26 @@ var argv = yargs
         demand: false,
         string: true
       }
+    },
+    (argv) => {
+      diff(argv);
+    }
+  )
+  .command(
+    'compile',
+    'Compiles the markdown files into HTML format, creates new file in the same folder',
+    {
+      f: {
+        demand: true,
+        string: true
+      },
+      m: {
+        demand: true,
+        string: true
+      }
+    },
+    (argv) => {
+      compile(argv);
     }
   )
   .help('h')
@@ -37,38 +57,83 @@ var argv = yargs
   .argv
 
 
+function compile(argv){
+  var fs = require('fs')
+  console.log(`Compiling ${argv.original} ${argv.modified}`);
+  var original = fs.readFileSync(argv.original, 'utf8');
+  var modified = fs.readFileSync(argv.modified, 'utf8');
 
-var fs = require('fs')
+  var unified = require('unified')
+  var markdown = require('remark-parse')
+  var html = require('remark-html')
 
-var output = argv.o || "out.md";
-
-console.log(output)
-
-var original = fs.readFileSync(argv.original, 'utf8');
-var modified = fs.readFileSync(argv.modified, 'utf8');
-
-const res = markdownDiff(original, modified);
-
-var unified = require('unified')
-var markdown = require('remark-parse')
-var html = require('remark-html')
-
-unified()
-  .use(markdown)
-  .use(html)
-  .process(res, function(err, file) {
-    if (err) {
-      return console.log(err);
-    }
-
-    let html = String(file)
-
-    html = html.replace(/<del>/g, `<del style="color:#a33;background:#ffeaea;text-decoration:line-through;">`); 
-    html = html.replace(/<ins>/g, `<ins style="color:darkgreen;background:#eaffea;">`); 
-
-    fs.writeFile(output, html, function (err,data) {
+  unified()
+    .use(markdown)
+    .use(html)
+    .process(original, function(err, file) {
       if (err) {
         return console.log(err);
       }
+
+      let html = String(file)
+
+      fs.writeFile(argv.original+".html", html, function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+      });
     });
-  })
+    
+    unified()
+    .use(markdown)
+    .use(html).process(modified, function(err, file) {
+      if (err) {
+        return console.log(err);
+      }
+
+      let html = String(file)
+
+      fs.writeFile(argv.modified+".html", html, function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    })
+
+}
+
+
+function diff(argv){
+  var fs = require('fs')
+
+  var output = argv.o || "out.html";
+
+  var original = fs.readFileSync(argv.original, 'utf8');
+  var modified = fs.readFileSync(argv.modified, 'utf8');
+
+  const res = markdownDiff(original, modified);
+
+  var unified = require('unified')
+  var markdown = require('remark-parse')
+  var html = require('remark-html')
+
+  unified()
+    .use(markdown)
+    .use(html)
+    .process(res, function(err, file) {
+      if (err) {
+        return console.log(err);
+      }
+
+      let html = String(file)
+
+      html = html.replace(/<del>/g, `<del style="color:#a33;background:#ffeaea;text-decoration:line-through;">`); 
+      html = html.replace(/<ins>/g, `<ins style="color:darkgreen;background:#eaffea;">`); 
+
+      fs.writeFile(output, html, function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    })
+}
